4e38144fd7aacae1d9496690e6e8ca15dd7d70a8
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-12-09 22:41:10 +0500

    commands added

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -27,7 +27,101 @@ const page = () => {
         </div>
       </div>
       <div className="max-w-[1280px] px-[15px] mt-[16px] w-full mx-auto">
-        <SimpleCarousel/>
+        <SimpleCarousel />
+      </div>
+      <div className="py-[16px]">
+
+      </div>
+      <div className="max-w-[1280px] px-[15px] mt-[16px] w-full mx-auto">
+        <div className="mb-4">
+          <h1 className="roboto-bold text-[24px] text-[#002f34] overflow-hidden mb-4 text-ellipsis whitespace-nowrap">All Categories</h1>
+          <div className="flex justify-start gap-x-[40.75px] gap-y-[16px] mb-[40px] items-center flex-wrap">
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/mobiles.8c768c96bfde33f18fcf5af2a8b9cf71.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">Mobiles</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/vehicles.29fb808d5118f0db56f68a39ce5392e2.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">Vehicles</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/property-for-sale.e3a00dbfdaa69fe5f713665f1069502f.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">Property for sale</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/property-for-rent.8436595fbaa90d47f0178006f57090a8.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">property for rent</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/electronics-home-appliances.46e034dd8adca44625c2c70e4d1b5984.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">Electronics and home appliances</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/bikes.4dcd02c49b2b83aa5b4d629d1e2b383e.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">Bikes</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/business-industrial-agriculture.704a6ffb9081bc94b11c102cc613670f.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">buisness, industrial and agricultural</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/services.dc6aef196c0403dc61b0ee813f66fa5b.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">Services</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/jobs.79e6136dda02111cf8e7afe26b9e0f93.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">jobs</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/animals.62d396e85f7523dbc8ff23889fdd5c31.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">animals</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/furniture-home-decor.66bcf157a53ea4c736a5b0af41219475.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">furniture and home decor</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/fashion-beauty.dd2cf7638c29b0e5c084a6673dd94dd7.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">fashion and beauty</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/books-sports-hobbies.6fee8d841b332d65a10f050f4a2ee1c8.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">Books, sports and hobbies</h1>
+            </div>
+            <div className="flex-col justify-center h-[156px]">
+              <div className="bg-transparent h-[88px] mx-auto w-[88px] rounded-full">
+                <img src="https://www.olx.com.pk/assets/kids.cd8d8864804f1c35dd6a7df68268a48d.png" alt="" />
+              </div>
+              <h1 className="text-center mt-2 text-[16px] roboto-bold capitalize leading-5 w-[102px]">kids</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
